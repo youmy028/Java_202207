ababa7eacc06d49a4c9086ece033ecfa690449fb
youmy028 <youmy028@naver.com>
2022-07-10 13:51:17 +0900

    게시물 작성 기능 구현, 스캐너 명령어 입력받기

--- src/com/it/java/ssg/Main.java
+++ src/com/it/java/ssg/Main.java
@@ -6,11 +6,32 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("명령어를 입력하세요)");
-		String command = sc.next();
-		System.out.println("입력된 명령어 :" + command);
-		int num = sc.nextInt();
-		System.out.println("입력된 명령어 :" + num);
+		int lastArticleId = 0; 
+		while (true) {
+			System.out.println("명령어를 입력하세요)");
+			String command = sc.nextLine();
+			command = command.trim();
+			if (command.length() == 0) {
+				continue;
+			}
+
+			if (command.equals("exit")) {
+				break;
+			}
+			if (command.equals("article write")) {
+				int id = lastArticleId+1;
+				lastArticleId = id;
+				System.out.printf("제목: ");
+				String 제목 = sc.nextLine();
+				System.out.printf("내용: ");
+				String 내용 = sc.nextLine();
+				System.out.printf("%d번째글이 생성되었습니다.\n", id);
+			} else if (command.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			} else {
+				System.out.printf("%s존재하지 않는 명령어입니다.\n", command);
+			}
+		}
 		sc.close();
 		System.out.println("== 프로그램 끝 ==");
 
